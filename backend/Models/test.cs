927458e1c45f1ba7e3cbd434c51d5c4501c35761
Eirik Andreas Johansen <eirikandreas.johansen@tietoevry.com>
2024-05-28 21:05:25 +0200

    test

---
+++ backend/Models/test.cs
@@ -0,0 +1,11 @@
+﻿namespace agile_dev.Models;
+
+public class test {
+    public string[] Summaries { get; set; }
+    public test() {
+        Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+    }
+}
