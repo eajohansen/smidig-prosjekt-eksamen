c2b71e3d08f745d18e601b4da44a39ace4f26d66
Leon1005 <borg.leon2@gmail.com>
2024-05-29 18:46:22 +0200

    allowJS and Started email validation

---
+++ frontend/src/validateEmail.js
@@ -0,0 +1,10 @@
+export const validateEmail = (email) => {
+  let re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (re.test(email)) {
+    return true;
+  } else {
+    return false;
+  }
+};
