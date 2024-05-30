04b37aefe99651152d3c05b519bc82b0c85992ea
Eirik Andreas Johansen <lifetap_@hotmail.com>
2024-05-30 13:48:57 +0200

    init context changes

--- backend/Repo/InitContext.cs
+++ backend/Repo/InitContext.cs
@@ -8,9 +8,7 @@ namespace agile_dev.Repo;
 public class InitContext : DbContext{
     private readonly IConfiguration _configuration;
     
-    public DbSet<User> User { get; set; }
-    public DbSet<Profile> Profile { get; set; }
-    public DbSet<Allergy> Allergy { get; set; }
+
     
     public InitContext(IConfiguration configuration) {
         _configuration = configuration;
@@ -18,13 +16,18 @@ public class InitContext : DbContext{
     public InitContext() {
         _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
     }
+    public DbSet<User> User { get; set; }
+    public DbSet<Profile> Profile { get; set; }
+    public DbSet<Allergy> Allergy { get; set; }
+    public DbSet<ContactPerson> ContactPerson { get; set; }
+    public DbSet<Event> Event { get; set; }
+    public DbSet<CustomField> CustomField { get; set; }
+    public DbSet<Organization> Organization { get; set; }
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
         if (!optionsBuilder.IsConfigured) {
             optionsBuilder.UseMySQL("Server=database,9999;Database=agile-project;User=root;Password=agileavengers;");
         }
         
     }
-
-  
-
 }
