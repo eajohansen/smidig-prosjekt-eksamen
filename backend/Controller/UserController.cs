9dd108b9dbbba498298ff48c7f7a681fa8daa9c6
Eirik Andreas Johansen <eirikandreas.johansen@tietoevry.com>
2024-05-30 09:06:00 +0200

    yes

---
+++ backend/Controller/UserController.cs
@@ -0,0 +1,43 @@
+using agile_dev.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace agile_dev.Controller {
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase {
+        private readonly UserService _userService;
+        public UserController(UserService userService) {
+            _userService = userService;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id) {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public async Task<IActionResult> AddUser() {
+            var result = await _userService.AddUserToDatabase();
+            return result;
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+        }
+    }
+}
