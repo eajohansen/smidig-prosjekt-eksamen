9af74ed6a7e0bf4a8e543cbdca70254ace5fb325
Eirik Andreas Johansen <eirikandreas.johansen@tietoevry.com>
2024-05-31 12:01:17 +0200

    Fixed up in auth part, and added logging for all requests.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,12 +1,10 @@
-using System.Configuration;
 using System.Text;
-using agile_dev.Models;
 using agile_dev.Repo;
 using agile_dev.Service;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
 namespace agile_dev;
@@ -17,11 +15,10 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddHttpLogging(logging => {
-            logging.LoggingFields = HttpLoggingFields.All;
+            logging.LoggingFields = HttpLoggingFields.Request;
             logging.RequestHeaders.Add("Referer");
             logging.ResponseHeaders.Add("MyResponseHeader");
         });
-        // CHANGE THIS TO BE MORE SPECIFIC FOR CORS, WILL FAIL WITH AUTH AS IS
         builder.Services.AddCors(
             options => {
                 options.AddPolicy("_frontendCorsPolicy",
@@ -37,8 +34,23 @@ public class Program
         builder.Services.AddDbContext<InitContext>(options =>
             options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
         builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<InitContext>();
+        
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => {
+                options.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Issuer"],
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
         builder.Services.AddAuthorization();
         var app = builder.Build();
+
         app.UseCors("_frontendCorsPolicy");
 
         app.MapIdentityApi<IdentityUser>();
@@ -50,6 +62,7 @@ public class Program
             InitContext dbContext = services.GetRequiredService<InitContext>();
             dbContext.Database.Migrate();
         }
+        app.UseHttpLogging();
         app.UseAuthorization();
         app.UseEndpoints(endpoints => {
             endpoints.MapControllers();
