08600bd15d23916368eecf00838b55223301daea
Leon1005 <borg.leon2@gmail.com>
2024-06-04 20:34:57 +0200

    started profile setup functionality to database

--- frontend/src/services/tempService.js
+++ frontend/src/services/tempService.js
@@ -1,14 +1,25 @@
-import {axiosInstance} from "../helper/axios.js";
+import { axiosInstance } from "./helpers";
 
 export const sendRegister = async (email, password) => {
   try {
-   await axiosInstance.post("register",
-       { email, password }
-   );
-    return "Success!"
+    const result = await axiosInstance.post("register", { email, password });
+    return result.status === 200;
   } catch (err) {
-    console.log(err.response.data.errors)
+    console.log(err.response.data.errors);
     return err.response.data.errors;
   }
 };
-
+export const sendUser = async (user) => {
+  try {
+    console.log(user);
+    const result = await axiosInstance.post("api/user/create", {
+      email: user.email,
+      firstname: user.firstName,
+      lastname: user.lastName,
+    });
+    return result.status === 200;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
