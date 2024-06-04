e9135ea746dffbd0f5cdd7bb55da8c44a1fd3697
Leon1005 <borg.leon2@gmail.com>
2024-06-04 21:46:09 +0200

    started login talk to backednd

--- frontend/src/services/tempService.js
+++ frontend/src/services/tempService.js
@@ -24,3 +24,12 @@ export const sendUser = async (user, allergyList) => {
     return err;
   }
 };
+export const sendLogin = async (email, password) => {
+  try {
+    const result = await axiosInstance.post("login", { email, password });
+    return result.status === 200;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
