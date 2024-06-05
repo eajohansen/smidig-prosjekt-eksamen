2492c17cf6341adf6f8bbd198720e18755e2e66c
Stefan <stefan.naeve@hotmail.com>
2024-06-05 22:24:45 +0200

    Added lots of null checks.

--- backend/Agile-dev/Controller/UserController.cs
+++ backend/Agile-dev/Controller/UserController.cs
@@ -13,16 +13,16 @@ namespace agile_dev.Controller {
         public UserController(UserService userService) {
             _userService = userService;
         }
-        
+
         /*
             Using Async methods to avoid blocking the main thread.
             This is important because the main thread is responsible for handling incoming requests.
-            
+
             Exceptions are thrown when an error occurs.
             This is important bacause we have to send the error code to the client.
-            
+
             Task<> is used because we are using async methods.
-            
+
             IActionResult is used because we are returning a response.
          */
 
@@ -36,7 +36,7 @@ namespace agile_dev.Controller {
                 if (result.Count == 0) {
                     return NoContent();
                 }
-        
+
                 return Ok(result);
             }
             catch (Exception exception) {
@@ -52,30 +52,32 @@ namespace agile_dev.Controller {
                 if (result == null) {
                     return NoContent();
                 }
-        
+
                 return Ok(result);
             }
             catch (Exception exception) {
                 return StatusCode(500, "Internal server Error: " + exception.Message);
             }
         }
-        
+
         // GET api/user/fetch/email/gunnar@gunnarsen.no
         [HttpGet("fetch/email/{email}")]
-        public async Task<IActionResult> FetchUserByEmail(string email) {
+        public async Task<IActionResult> FetchUserByEmail(string? email) {
+            if (email == null) {
+                return BadRequest("Email is null");
+            }
             try {
                 User? result = await _userService.FetchUserByEmail(email);
                 if (result == null) {
                     return NoContent();
                 }
-        
+
                 return Ok(result);
             }
             catch (Exception exception) {
                 return StatusCode(500, "Internal server Error: " + exception.Message);
             }
         }
-        
 
         #endregion
 
@@ -83,61 +85,78 @@ namespace agile_dev.Controller {
 
         // POST api/user/create
         [HttpPost("create")]
-        public async Task<IActionResult> AddUser(User user) {
+        public async Task<IActionResult> AddUser(User? user) {
+            if (user == null) {
+                return BadRequest("User is null");
+            }
             try {
                 bool isAdded = await _userService.AddUserToDatabase(user);
                 if (!isAdded) {
                     // Could create user, because request is bad
                     return BadRequest();
                 }
+
                 return Ok();
             }
             catch (Exception exception) {
                 return StatusCode(500, "Internal server error: " + exception.Message);
             }
         }
-        
+
         // POST api/user/organizer/create/5/1
-        [HttpPost("organizer/create/{loggedInUserId}/{organizationId}")]
-        public async Task<IActionResult> AddOrganizer([FromRoute] int loggedInUserId, [FromBody] User user, [FromRoute] int organizationId) {
+        [HttpPost("organizer/create/{loggedInUserId}")]
+        public async Task<IActionResult> AddOrganizer([FromRoute] int loggedInUserId, [FromBody] User? user) {
+            int organizationId = 1;
+            if (user == null) {
+                return BadRequest("User is null");
+            }
             try {
                 bool isAdded = await _userService.AddUserAsOrganizer(loggedInUserId, user, organizationId);
                 if (!isAdded) {
                     // Could not add user as organizer, because request is bad
                     return BadRequest();
                 }
+
                 return Ok();
             }
             catch (Exception exception) {
                 return StatusCode(500, "Internal server error: " + exception.Message);
             }
         }
-        
+
         // POST api/user/follower/create/1
         [HttpPost("follower/create/{organizationId}")]
-        public async Task<IActionResult> AddFollower([FromBody] User user, [FromRoute] int organizationId) {
+        public async Task<IActionResult> AddFollower([FromBody] User? user, [FromRoute] int organizationId) {
+            if (user == null) {
+                return BadRequest("User is null");
+            }
             try {
                 bool isAdded = await _userService.AddUserAsFollower(user, organizationId);
                 if (!isAdded) {
                     // Could not add user as follower, because request is bad
                     return BadRequest();
                 }
+
                 return Ok();
             }
             catch (Exception exception) {
                 return StatusCode(500, "Internal server error: " + exception.Message);
             }
         }
-        
+
         // POST api/user/event/add/3
         [HttpPost("event/add/{eventId}")]
-        public async Task<IActionResult> AddUserEvent([FromBody] User user, [FromRoute] int eventId) {
+        public async Task<IActionResult> AddUserEvent([FromBody] User? user, [FromRoute] int eventId) {
+            if (user == null) {
+                return BadRequest("User is null");
+            }
             try {
                 bool isAdded = await _userService.AddUserToEvent(user, eventId);
                 if (!isAdded) {
                     // Could not add user to event, because request is bad
                     return BadRequest();
                 }
+
                 return Ok();
             }
             catch (Exception exception) {
@@ -151,12 +170,16 @@ namespace agile_dev.Controller {
 
         // PUT api/user/update/5
         [HttpPut("update/{id}")]
-        public async Task<IActionResult> UpdateUser(User user) {
+        public async Task<IActionResult> UpdateUser(User? user) {
+            if (user == null) {
+                return BadRequest("User is null");
+            }
             try {
                 bool isAdded = await _userService.UpdateUser(user);
                 if (!isAdded) {
                     return BadRequest();
                 }
+
                 return Ok();
             }
             catch (Exception exception) {
@@ -168,12 +191,16 @@ namespace agile_dev.Controller {
         // "AdminUser" in this context is the user that is making the request
         // "id" is the id of the user that is being made admin
         [HttpPut("admin/update/{id}")]
-        public async Task<IActionResult> MakeUserAdmin([FromBody] User adminUser, [FromRoute] int id) {
+        public async Task<IActionResult> MakeUserAdmin([FromBody] User? adminUser, [FromRoute] int id) {
+            if (adminUser == null) {
+                return BadRequest("AdminUser is null");
+            }
             try {
                 bool makeUserAdmin = await _userService.MakeUserAdmin(adminUser, id);
                 if (!makeUserAdmin) {
                     return Unauthorized();
                 }
+
                 return Ok();
             }
             catch (Exception exception) {
@@ -187,13 +214,18 @@ namespace agile_dev.Controller {
 
         // DELETE api/User/delete
         [HttpDelete("delete")]
-        public IActionResult Delete(User user) {
+        public IActionResult Delete(User? user) {
+            if (user == null) {
+                return BadRequest("User is null");
+            }
+
             try {
-                bool isDeleted = _userService.DeleteUser(user);
+                bool isDeleted = _userService.DeleteUser(user).Result;
                 if (!isDeleted) {
-                    // Could not delete user, because frontend is stoopid
+                    // Could not delete user, because request is bad
                     BadRequest();
                 }
+
                 return Ok();
             }
             catch (Exception exception) {
