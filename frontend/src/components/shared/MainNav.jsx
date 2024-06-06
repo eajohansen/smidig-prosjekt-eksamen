4960eaf0c675419b8889635d24ccf16e752466fc
Leon1005 <borg.leon2@gmail.com>
2024-06-06 14:27:37 +0200

    navbar styles

--- frontend/src/components/shared/MainNav.jsx
+++ frontend/src/components/shared/MainNav.jsx
@@ -1,8 +1,33 @@
 import { Link } from "react-router-dom";
-
+import "bootstrap-icons/font/bootstrap-icons.css";
 export const MainNav = ({ version }) => {
   switch (version) {
     case "user":
+      return (
+        <header>
+          <div className="navHeader">
+            <Link to="/" className="navItem">
+              <i className="bi bi-house"></i> <p>Hjem / TEST</p>
+            </Link>
+
+            <Link to="profile" className="navItem">
+              <i className="bi bi-person"></i> <p>Min Profil</p>
+            </Link>
+
+            <Link to="tickets" className="navItem">
+              <i className="bi bi-ticket"></i> <p>Biletter</p>
+            </Link>
+
+            <Link to="events" className="navItem">
+              <i className="bi bi-calendar-event"></i> <p>Arrangementer</p>
+            </Link>
+            <Link to="/" className="navItem">
+              <i class="bi bi-box-arrow-left"></i> <p>Logg ut</p>
+            </Link>
+          </div>
+        </header>
+      );
+    case "organizer":
       return (
         <header>
           <div className="navHeader">
@@ -14,27 +39,13 @@ export const MainNav = ({ version }) => {
               Min Profil
             </Link>
 
-            <Link to="tickets" className="navItem">
-              Biletter
-            </Link>
-
             <Link to="events" className="navItem">
               Arrangementer
             </Link>
           </div>
         </header>
       );
-    case "organizer":
-      return (
-        <header>
-          <div>whatever</div>
-        </header>
-      );
     case "dummy":
-      return (
-        <header>
-          <div>whatever</div>
-        </header>
-      );
+      return <header></header>;
   }
 };
