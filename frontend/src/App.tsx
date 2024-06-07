e448d83ae6c364b7c6654c42f20d997b0aede3c0
Trine H. Davidsen <114672051+THD-94@users.noreply.github.com>
2024-06-07 19:25:59 +0200

    HTML ProfilePage and OrgProfilePage is up
    Mye likt fra Profileform.
    Ikke noe funksjon (da ble det bare en hvit side)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,11 @@ import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainNav } from "./components/shared/MainNav";
 import { HomePage } from "./pages/HomePage";
+import { OrgProfilePage } from "./pages/OrgProfilePage";
 import { ProfilePage } from "./pages/ProfilePage";
 import { TicketsPage } from "./pages/TicketsPage";
 import { EventsPage } from "./pages/EventsPage";
+
 
 function App() {
   async function testClick() {
@@ -16,13 +18,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <MainNav version={"user"} />
+        <MainNav version={"organizer"} />
         <main>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="profile" element={<ProfilePage />} />
             <Route path="tickets" element={<TicketsPage />} />
             <Route path="events" element={<EventsPage />} />
+            <Route path="orgProfile" element={<OrgProfilePage/>} />
           </Routes>
 
           {/* <ProfileForm userEmail={"Eksempel@epost.no"} /> */}
