c9bd9f91587008d35109c6fdc167c10cea6d2f9b
Trine H. Davidsen <114672051+THD-94@users.noreply.github.com>
2024-06-09 18:23:39 +0200

    fixed some css positions

--- frontend/src/components/EventItem.jsx
+++ frontend/src/components/EventItem.jsx
@@ -9,22 +9,29 @@ export const EventItem = (props) => {
             <img className="eventImage"
             src="https://images.unsplash.com/photo-1717684566059-4d16b456c72a?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="random"/>
-        </div>
         <div className="eventDetails">
         <h3>{title}</h3>
         <p>{date}</p>
         </div>
-        <div className="ticketInfo">
+        </div>
+        <div className="ticketInfoContainer">
           <h3>Billetter</h3>
+          <div className="ticketInfo">
+
+          <div className="">
           <p>Solgt</p>
           <h2>{sold}</h2>
-          <p>Venteliste</p>
-          <h2 className="waitlist">{waitlist}</h2>
           <p>Ledig</p>
           <h2 className="available">{available}</h2>
+          </div>
+          <div className="WaitinglistContainer">
+          <p>Venteliste</p>
+          <h2 className="waitlist">{waitlist}</h2>
+          </div>
+          </div>
           <div className="buttonContainer">
               <button className="viewRegistrationsButton">Vis påmeldte</button>
-              <button>Se arrangement</button>
+              <button className="seeEventButton ">Se arrangement</button>
           </div>
         </div>
     </article>
