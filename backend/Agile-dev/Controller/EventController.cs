02a232d5d1a9060a68ae5be923ff169ad9876a75
Eirik Andreas Johansen <eirikandreas.johansen@tietoevry.com>
2024-06-10 12:59:42 +0200

    Fetch all url shortned

--- backend/Agile-dev/Controller/EventController.cs
+++ backend/Agile-dev/Controller/EventController.cs
@@ -102,8 +102,8 @@ namespace Agile_dev.Controller {
             }
         }
 
-        // GET api/event/fetch/id/5
-        [HttpGet("fetch/id/{id}")]
+        // GET api/event/fetch/5
+        [HttpGet("fetch/{id}")]
         public async Task<IActionResult> FetchEventById(int id) {
             try {
                 Event? result = await _eventService.FetchEventById(id);
