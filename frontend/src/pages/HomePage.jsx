6b07ae3e6542ebc04e42dae36f4f7a629ff7fcf3
jbhansen87 <111732821+jbhansen87@users.noreply.github.com>
2024-06-10 12:33:41 +0200

    Added HomePage.jsx, homepage.css, followersBox.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,32 @@
 import React from "react";
-import LoginPopup from "../components/LoginPopup";
-import { ProfileForm } from "../components/ProfileForm";
-
+//import {EventItem} from "../components/EventItem.jsx";
+//import {events} from "./EventsPage.jsx";
+export const follower = [
+  {
+    followersTotal: 122,
+    followersLastMonth: 39,
+    growth: 83,
+  },
+];
 export const HomePage = () => {
-   return <LoginPopup />;
-  //return <ProfileForm />;
+  // return <LoginPopup />; ??
+  return (
+    <>
+      <header className="homePageHeader">
+        <h1 className="homePageTitle">HJEM</h1>
+      </header>
+      <main className="homePageContainer">
+        <div className="followerContainer">
+          <FollowersBox
+            followers={follower.followersTotal}
+            followersLM={follower.followersLastMonth}
+            growth={follower.growth}
+          />
+        </div>
+        <div className="eventContainer"></div>
+        <div className="createEventContainer"></div>
+        <div className="calendar"></div>
+      </main>
+    </>
+  );
 };
