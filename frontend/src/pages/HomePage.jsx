ec505e76ff52265cb3ca01dd6baa90c2d41fe064
Trine H. Davidsen <114672051+THD-94@users.noreply.github.com>
2024-06-10 13:41:53 +0200

    fixing posision

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,31 +1,54 @@
 import React from "react";
-//import {EventItem} from "../components/EventItem.jsx";
+import { EventItem } from "../components/EventItem.jsx";
 //import {events} from "./EventsPage.jsx";
-export const follower = [
-  {
-    followersTotal: 122,
-    followersLastMonth: 39,
-    growth: 83,
-  },
-];
+import { MainNav } from "../components/shared/MainNav";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+// const follower= [
+//   {
+//     followersTotal: 122,
+//     followersLastMonth: 39,
+//     growth: 83
+//   }
+// ];
 export const HomePage = () => {
-  // return <LoginPopup />; ??
+  //  return <LoginPopup />;
   return (
     <>
+      <MainNav />
       <header className="homePageHeader">
-        <h1 className="homePageTitle">HJEM</h1>
+        <h1 className="homePageTitle">Hjem</h1>
       </header>
+
       <main className="homePageContainer">
-        <div className="followerContainer">
-          <FollowersBox
-            followers={follower.followersTotal}
-            followersLM={follower.followersLastMonth}
-            growth={follower.growth}
-          />
+        <div className="eventContainer">
+          <label htmlFor="">Førstkommende Arrangement</label>
+          <div className="eventObject">
+            <EventItem />
+          </div>
         </div>
-        <div className="eventContainer"></div>
-        <div className="createEventContainer"></div>
-        <div className="calendar"></div>
+
+        <div className="followerContainer">
+          <label htmlFor="">Vekst</label>
+          <div>
+            <label htmlFor="">Følgere</label>
+            {/* <FollowersBox
+                      followers={follower.followersTotal}
+                      followersLM={follower.followersLastMonth}
+                      growth={follower.growth}
+                      /> */}
+            <button>Vis liste over følgere</button>
+          </div>
+        </div>
+        <div className="calendar">
+          <label htmlFor="">Oversikt</label>
+          <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+        </div>
+
+        <div className="createEventContainer">
+          <label htmlFor="">Legg til nytt arrangement</label>
+          <button></button>
+        </div>
       </main>
     </>
   );
