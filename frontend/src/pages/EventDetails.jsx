adc1815494727c7c74325c793ce9845be6fb7178
Leon1005 <borg.leon2@gmail.com>
2024-06-11 17:26:10 +0200

    manual merge
    manually merged Routing-trip-frontend with the new HomePage and styles for Event Details page

--- frontend/src/pages/EventDetails.jsx
+++ frontend/src/pages/EventDetails.jsx
@@ -1,3 +1,4 @@
+import "../css/EventDetails.css";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getEventById } from "../services/tempService";
@@ -30,25 +31,76 @@ export const EventDetails = () => {
           src="https://images.unsplash.com/photo-1717684566059-4d16b456c72a?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
+        <div className="infoContainer">
+          <div className="infoItem">
+            <p>Ledige Plasser:</p>
+            <p>{event.capacity}</p>
+          </div>
+          <div className="infoItem">
+            <p>Aldersgrense:</p>
+            <p>{event.ageLimit}</p>
+          </div>
+          <div className="infoItem">
+            <p>privat?: </p>
+            <p>{event.private ? "ja" : "nei"}</p>
+          </div>
+          <div className="infoItem">
+            <p>Organisasjon:</p>
+            <p> {event?.organization?.name}</p>
+          </div>
+          <div className="infoItem">
+            <p>Kontaktperson:</p>
+            <p> {event?.contactPerson?.name}</p>
+          </div>
+        </div>
       </div>
       <div className="eventDetailsRight">
-        <h1>{event.title}</h1>
-        <p>DATO</p>
-        <p>Beskrivelse: {event.description}</p>
-        <p>Antall plasser: {event.capacity}</p>
-        <p>Aldersgrense: {event.ageLimit}</p>
-        <p>privat?: {event.private ? "ja" : "nei"}</p>
-        <p>publisert?: {event.published ? "ja" : "nei"}</p>
-        <p>organisasjon: {event?.organization?.name}</p>
-        <p>lokasjon: {event?.place?.location}</p>
-        <p>kontaktperson: {event?.contactPerson?.name}</p>
-        <p>start: {event.startTime}</p>
-        <p>slutt: {event.endTime}</p>
+        <div className="eventInfo">
+          <h1>{event.title}</h1>
+          <div className="dateTimePlace">
+            <div className="dateTime">
+              <div className="dateItem">
+                <i className="bi bi-calendar3 icon"></i>
+                <p>dATO</p>
+              </div>
+              <div className="timeItem">
+                <i className="bi bi-clock icon"></i>
+                <p>
+                  17:00 - 19:00
+                  {/* {event.startTime}
+                  {event.endTime} */}
+                </p>
+              </div>
+            </div>
+            <div className="place">
+              <i className="bi bi-geo-alt icon"></i>
+              <p>{event.place.location}</p>
+            </div>
+          </div>
+        </div>
+        <div className="eventText">
+          <p>{event.description}</p>
+        </div>
         <div className="btnDiv">
-          <button>Rediger</button>
-          <button>Publiser</button>
+          <button className="buttons">Rediger</button>
+          <button className="publishBtn buttons">Publiser</button>
         </div>
       </div>
     </div>
   );
 };
+
+{
+  /* <h1>{event.title}</h1>
+<p>DATO</p>
+<p>Beskrivelse: {event.description}</p>
+<p>Antall plasser: {event.capacity}</p>
+<p>Aldersgrense: {event.ageLimit}</p>
+<p>privat?: {event.private ? "ja" : "nei"}</p>
+<p>publisert?: {event.published ? "ja" : "nei"}</p>
+<p>organisasjon: {event?.organization?.name}</p>
+<p>lokasjon: {event?.place?.location}</p>
+<p>kontaktperson: {event?.contactPerson?.name}</p>
+<p>start: {event.startTime}</p>
+<p>slutt: {event.endTime}</p> */
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,55 @@
+import "../css/homepage.css";
+import { EventItem } from "../components/EventItem.jsx";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { Link } from "react-router-dom";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
-  //return <ProfileForm />;
+  return (
+    <>
+      <main className="homePageContainer">
+        <div className="eventHomeContainer">
+          <label htmlFor="">Førstkommende Arrangement</label>
+          <div className="eventObject">
+            <EventItem />
+          </div>
+        </div>
+        <div className="followerContainer">
+          <label htmlFor="">Vekst</label>
+          <div className="vekstobject">
+            <h4>Følgere</h4>
+            <div className="infoFollowers">
+              <div>
+                <p>Antall følgere</p>
+                <p>5</p>
+                <p>følgere forrige måned</p>
+                <p>5</p>
+              </div>
+              <div>
+                <p>vekst</p>
+                <p>9</p>
+              </div>
+            </div>
+            <button className="seeEventButton showFollowers">
+              Vis liste over følgere
+            </button>
+          </div>
+        </div>
+        <div className="calendar">
+          <label htmlFor="">Oversikt</label>
+          <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+        </div>
+        <div className="createEventContainer">
+          <label className="createEventTxt" htmlFor="">
+            Legg til nytt arrangement
+          </label>
+          <Link to={"createEvent"}>
+            <button className="createEventButton">
+              <i className="bi bi-plus-circle circleIcon"></i>
+            </button>
+          </Link>
+        </div>
+      </main>
+    </>
+  );
 };
